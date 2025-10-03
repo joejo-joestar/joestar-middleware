03d91c1b44c43755caeeef8f02b82ab069591a1c
Joe <joecn2704+github@gmail.com>
2025-10-03 13:35:17 +0400

    updated to use the pug viewer (jade previously)

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var githubRouter = require('./routes/github');
@@ -14,18 +13,12 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// Enable CORS for the frontend hosted at joestar.vercel.app
-app.use(cors({
-  origin: 'https://joestar.vercel.app',
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
